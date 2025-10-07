bfa81e73a9d881919d193007a85f138869789700
Zaid Dweiri <zaiddweiri962@gmail.com>
2025-10-07 20:17:03 +0300

    finsinh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lms-alhamzeh.fly.storage.tigris.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
